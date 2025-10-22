d20f042e9ed3263b4528368570c2e192c7563b77
scm-idea-95 <29284204+madmoosey@users.noreply.github.com>
2025-10-21 20:35:11 -0400

    Health Endpoint

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,6 +24,10 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
+
+from django.http import JsonResponse
+def health(request): return JsonResponse({"status": "ok"})
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     # path("api/finance/", include(finance_urlpatterns)),  # your app
@@ -32,6 +36,7 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("api/health/", health),
 ]
 
 if settings.DEBUG:
